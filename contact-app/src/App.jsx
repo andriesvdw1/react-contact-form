660d597b823bfebea552d5d6d1d887dc6d32b0e0
andriesvdw1 <andriesvdw1@gmail.com>
2025-05-06 09:05:42 +0200

    Add contact form component to App component to make it display on the first page in the browser.

--- contact-app/src/App.jsx
+++ contact-app/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-
+import ContactForm from './components/ContactForm/ContactForm'
 function App() {
 
 
   return (
     <>
-      
+      <ContactForm />
     </>
   )
 }
