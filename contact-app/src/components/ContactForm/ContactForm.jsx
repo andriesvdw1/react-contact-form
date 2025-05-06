9612b16c6f040475e0910ffac0e0d71ec385773b
andriesvdw1 <andriesvdw1@gmail.com>
2025-05-06 09:41:08 +0200

    apply some styling to input box and form

--- contact-app/src/components/ContactForm/ContactForm.jsx
+++ contact-app/src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useForm} from 'react-hook-form';
+import './ContactForm.css';
 function ContactForm() {
   const { register, handleSubmit, formState } = useForm();  
   const onSubmit = async (data) => {
@@ -10,6 +11,7 @@ function ContactForm() {
         <h1>Contact me:</h1>
         <label htmlFor="inputMessage"></label>
         <input {...register('inputMessage', {required: true})}
+        className='input-message'
         type="text" placeholder='message' id='inputMessage' />
         <button type='submit'>Send</button>
     </form>
